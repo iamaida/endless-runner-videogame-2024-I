36ef2828ad7455c50f2a37839adcc66a2659bcee
unknown <iamaidamina@gmail.com>
2024-04-09 23:28:32 -0500

    adding sapphire gem collection funtionality with FBX audio

--- Assets/Scripts/Environment/GenerateLevel.cs
+++ Assets/Scripts/Environment/GenerateLevel.cs
@@ -24,7 +24,7 @@ public class GenerateLevel : MonoBehaviour
     {
         secNum = Random.Range(0, 3);
         Instantiate(section[secNum], new Vector3(0, 0, zPos), Quaternion.identity);
-        zPos += 50;
+        zPos += 10;
         yield return new WaitForSeconds(2);
         creatingSection = false;
     }
