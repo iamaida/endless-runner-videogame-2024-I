5804d045e054e6d1d75a21eb0da81ba831012950
unknown <iamaidamina@gmail.com>
2024-04-09 21:41:30 -0500

    creating infinite endless runway generation

---
+++ Assets/Scripts/Environment/GenerateLevel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateLevel : MonoBehaviour
+{
+    public GameObject[] section;
+    public int zPos = 10;
+    public bool creatingSection = false;
+    public int secNum;
+    // Start is called before the first 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!creatingSection)
+        {
+            creatingSection = true;
+            StartCoroutine(GenerateSection());
+        }
+    }
+
+    IEnumerator GenerateSection()
+    {
+        secNum = Random.Range(0, 3);
+        Instantiate(section[secNum], new Vector3(0, 0, zPos), Quaternion.identity);
+        zPos += 50;
+        yield return new WaitForSeconds(2);
+        creatingSection = false;
+    }
+}
