a0dacd9880588a32f99ae0418fcfe72edf035f60
unknown <iamaidamina@gmail.com>
2024-04-12 00:04:14 -0500

    adding audio to countdown and setting to jolleen just can move left and right when countdown finish

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -7,23 +7,26 @@ public class PlayerMove : MonoBehaviour
     // Start is called before the first frame update
     public float moveSpeed = 5;
     public float leftRightSpeed = 4;
-
+    static public bool canMove = false;
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (canMove)
         {
-            if (this.gameObject.transform.position.x > LevelBoundary.leftSide)
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
             {
-                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+                if (this.gameObject.transform.position.x > LevelBoundary.leftSide)
+                {
+                    transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+                }
             }
-        }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             {
-                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+                if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
+                {
+                    transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+                }
             }
         }
     }
