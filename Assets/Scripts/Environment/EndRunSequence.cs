98f76cbedd52c6ca2ff78184f2965677040e019d
unknown <iamaidamina@gmail.com>
2024-04-14 05:46:23 -0500

    creating final scoring board and a main menu in a new scene

--- Assets/Scripts/Environment/EndRunSequence.cs
+++ Assets/Scripts/Environment/EndRunSequence.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndRunSequence : MonoBehaviour
 {
@@ -17,11 +18,13 @@ public class EndRunSequence : MonoBehaviour
 
     IEnumerator EndSequence()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);
         liveSapphires.SetActive(false);
         liveDis.SetActive(false);
         endScreen.SetActive(true);
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);
         fadeOut.SetActive(true);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(0);
     }
 }
